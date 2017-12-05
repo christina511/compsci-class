9fc4d4cccb8d3ad43ac285c9e30b7466c4c8d66e
Christina L <christina511ny@gmail.com>
2017-12-04 22:42:22 -0500

    Create project#5-12-4-2017.py

---
+++ projects/project#5-12-4-2017.py
@@ -0,0 +1,36 @@
+import math
+
+addMinutes = int(input("How long is each session (in minutes)?"))
+
+newHours = 8
+newMinutes = 0
+session = 0
+extraHours=0
+
+fiveMinutes = input('Want to add 5 minutes in between each session? Enter "yes" or "no": ')
+
+while (fiveMinutes!="yes" and fiveMinutes!= "no"):
+  fiveMinutes = input('Want to add 5 minutes in between each session? Enter "yes" or "no": ')
+
+if (fiveMinutes == "yes"):
+  fiveMinutes = True
+if (fiveMinutes == "no"):
+  fiveMinutes = False
+
+while (session <8):
+  session+=1
+  hours = newHours%12
+  minutes = newMinutes
+  if ((newMinutes + addMinutes) >=60):
+    extraHours = 1
+  else:
+    extraHours =0
+  newHours = (newHours + extraHours + addMinutes//60)%12
+  newMinutes = (minutes + addMinutes%60)%60
+  if(hours%12 == 0):
+    hours = 12
+  if(newHours%12 ==0):
+    newHours = 12
+  print("session: " + str(session) +'  '+ str(hours) +":" + '{0:02}'.format(minutes%60) + " - " + str(newHours) + ":" + '{0:02}'.format(newMinutes%60)) 
+  if (fiveMinutes == True):
+    newMinutes +=5
