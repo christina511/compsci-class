950ae9d3b1486b772d5fa0f3cfd6728eb009928b
Christina L <christina511ny@gmail.com>
2017-12-04 23:05:40 -0500

    Create 2017-12-01.py

---
+++ practice/2017-12-01.py
@@ -0,0 +1,18 @@
+#program to check if a number is prime (unfinished)
+
+import math
+num = int(input("Input a number to see if it is prime: "))
+mid = int(math.sqrt(num))+1
+divisor = 1
+
+while (divisor != mid):
+  
+  
+  
+  
+'''
+if (num%(range(2,mid)) == 0):
+  print("The number is prime!")
+else:
+  print("The number is not prime.")
+  '''
