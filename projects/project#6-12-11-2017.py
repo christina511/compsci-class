18bcc88b02c7a625341ed57bc3d0e7ef38f69343
Christina L <christina511ny@gmail.com>
2017-12-10 19:40:20 -0500

    Update project#6-12-11-2017.py

--- projects/project#6-12-11-2017.py
+++ projects/project#6-12-11-2017.py
@@ -4,6 +4,7 @@ import math
 turn=0
 p1= 0
 newp1=0
+bankp1=0
 p2 = 0
 p1action = "roll"
 
@@ -12,11 +13,13 @@ while (p1<= 100 or p2<= 100):
   while (p1action !="hold" and p1!= 1):
     p1 = newp1
     p1 = random.randint(1,6)
-    newp1= newp1 +p1
-    print("Turn:" + str(turn) +'\nPlayer 1 rolled a ' + str(p1) +". Your temporary total is now " + str(newp1))
-    p1action = input('Would you like to "roll" or "hold" ?')
-  if (p1action== "hold"):
-    newp1 = newp1
-    print("You accumulated a score of " + str(newp1) + " in turn " + str(turn))
-  if (p1==1):
-    newp1 =0
+    if (p1==1):
+      bankp1 = bankp1 +0
+      print("Turn:" + str(turn) +'\nPlayer 1 rolled a ' + str(p1) +". You accumulated 0 points this turn. Your bank total is now " + str(bankp1))
+    else:
+      newp1= newp1 +p1
+      print("Turn:" + str(turn) +'\nPlayer 1 rolled a ' + str(p1) +". Your temporary total is now " + str(newp1))
+      p1action = input('Would you like to "roll" or "hold" ?')
+    if (p1action== "hold"):
+      bankp1 = newp1
+      print("You accumulated a score of " + str(bankp1) + " in turn " + str(turn))
